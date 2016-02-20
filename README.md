d11a42e29232a4f8344bb6306e45ea3db87a27db
Xiaoli Wu <xrayxiaoli@gmail.com>
2016-02-20 10:06:44 +0800

    bump() 0.0.1

---
+++ README.md
@@ -0,0 +1,94 @@
+# node-wegene
+
+Node.js SDK for [WeGene](https://www.wegene.com)
+
+## Installation
+
+    $ npm install node-wegene
+
+## Usage
+
+#### Set Access Token
+
+To query data from WeGene, one need a valid OAuth2 access token authorized by
+the user. Node.js Express apps can use `passport-wegene` for implementing the
+OAuth2 flow.
+
+
+```javascript
+var wegene = require('node-wegene');
+var config = wegene.configuration;
+config.oAuthAccessToken = '<A Valid Access Token With Proper Scope>';
+```
+
+#### Get Basic User Info
+
+Use `wegene.User.getUser()` to get user data
+
+```javascript
+var user = wegene.User
+user.getUser(function(error, result){
+  if(error){ return console.log(error) }
+  console.log('User Info:\n', JSON.stringify(result));
+});
+```
+
+#### Get Gene Allele Info
+
+Use `wegene.Allele.getAllele(profile_id, callback)` to get allele data
+
+```javascript
+var allele = wegene.Allele;
+allele.getAllele(profile_id, ['rs182549'], function(error, result){
+  if(error){ return console.log(error) }
+  console.log('Allele Data:\n', JSON.stringify(result));
+});
+```
+
+#### Get Sport Advice
+
+Use `wegene.Sport.getAdvice(profileId, sex, age, height, weight, healthTarget, callback)` to get sport advice
+
+```javascript
+var sport = wegene.Sport;
+sport.getAdvice(profile_id, 'male', 26, 174, 84, 'slimming', function(error, result){
+  if(error){ return console.log(error) }
+  console.log('Sport Advice:\n', JSON.stringify(result));
+});
+```
+
+#### Get Health Data
+
+Use `wegene.Health.getRisk(profileId, reportId, callback)` to get health risk info
+Use `wegene.Health.getDrug(profileId, reportId, callback)` to get drug response info
+Use `wegene.Health.getCarrier(profileId, reportId, callback)` to get disease carrier info
+
+```javascript
+var health = wegene.Health;
+health.getDrug(profile_id, 158,function(error, result){
+  if(error){ return console.log(error) }
+  console.log('Health Data:\n', JSON.stringify(result));
+});
+```
+
+## Documentation
+  - [Full WeGene API Document](https://api.wegene.com/docs/)
+
+
+## Credits
+  - [Eddie Wu](https://xraywu.github.io)
+
+## Thanks
+  - The SDK is modified on top of the code auto-generated using [APIMATIC](https://apimatic.io). Huge thanks to the team as this is a real life saver!!
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2016 Eddie Wu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- example/query/example.js
+++ example/query/example.js
@@ -1,21 +1,48 @@
+/**
+ * node-wegene-example
+ *
+ * This is an example showcase how to use node-wegene to query WeGene data
+ */
+
 var async = require('async');
 var wegene = require('../../lib/node-wegene');
 
+/**
+ * Config WeGene access token
+ * Set oAuthAccessToken as a valid access token authorized by the user
+ * Node.js Express apps can use passport-wegene package
+ */
 var config = wegene.configuration;
 config.oAuthAccessToken = '<A Valid Access Token With Proper Scope>';
 
 async.waterfall([
-	// Query user's genetic test profile id first for further query
+	/**
+	 * Endpoint for basic user information
+	 * @param {function} callback    Required parameter: Callback function in the form of function(error, response)
+	 *
+	 * @return {User}
+	 */
 	function(callback){
 		var user = wegene.User;
     user.getUser(function(error, result){
       if(error){ return callback(error.message) }
 			console.log('User Info:\n', JSON.stringify(result));
 			console.log('----------');
+			// A user can have multiple genetic test profiles related to one account, each has a profile id with different analysis
       callback(null, result.profiles[0].id);
     });
   },
-	// Query report for specific health risk/drug response/disease carrier
+	/**
+	 * Drug response based on genetic information
+	 * @param {string} profileId    Required parameter: Genetic profile Id
+	 * @param {string} reportId    Required parameter: Report Id for the specific drug response to look
+	 * @param {function} callback    Required parameter: Callback function in the form of function(error, response)
+	 *
+	 * @return {mixed}
+	 *
+	 *
+	 * ** Same usage for querying Health Risk (health.getRisk()) and Disease Carrier (health.getCarrier())
+	 */
 	function(profile_id, callback){
 		var health = wegene.Health;
 		health.getDrug(profile_id, 158,function(error, result){
@@ -25,7 +52,18 @@ async.waterfall([
 			callback(null, profile_id);
 		});
 	},
-	// Query report for specific health risk/drug response/disease carrier
+	/**
+	 * Sports advice based on genetic and individual information
+	 * @param {string} profileId    Required parameter: Genetic profile Id
+	 * @param {string} sex    Required parameter: Sex of the user
+	 * @param {int} age    Required parameter: Age of the user
+	 * @param {int} height    Required parameter: Height of the user
+	 * @param {int} weight    Required parameter: Weight of the user
+	 * @param {string} healthTarget    Required parameter: Target for sport, only supporting slimming now
+	 * @param {function} callback    Required parameter: Callback function in the form of function(error, response)
+	 *
+	 * @return {SportAdvice}
+	 */
 	function(profile_id, callback){
 		var sport = wegene.Sport;
 		sport.getAdvice(profile_id, 'male', 26, 174, 84, 'slimming', function(error, result){
@@ -35,7 +73,14 @@ async.waterfall([
 			callback(null, profile_id);
 		});
 	},
-	// Query report for specific health risk/drug response/disease carrier
+	/**
+	 * Allele data
+	 * @param {string} profileId    Required parameter: Genetic Profile Id
+	 * @param {array} locations    Required parameter: Allele locations in array
+	 * @param {function} callback    Required parameter: Callback function in the form of function(error, response)
+	 *
+	 * @return {mixed}
+	 */
 	function(profile_id, callback){
 		var allele = wegene.Allele;
 		allele.getAllele(profile_id, ['rs182549'], function(error, result){
@@ -45,5 +90,6 @@ async.waterfall([
 			callback(null, 'Query Completed!');
 		});
 	}],function(error, result){
+		if(error){ console.log(error) }
 		console.log(result);
 });
